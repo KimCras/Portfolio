ac6ba327bc501d672c861c09bccf5f1ec0326477
Kimberly <kira0001@stud.kea.dk>
2024-01-01 21:41:11 +0100

    got burgermenu working

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleMenu() {
-  var menu = document.querySelector(".menu");
-  menu.style.display = menu.style.display === "block" ? "none" : "block";
-}
+document.querySelector(".burger-menu").addEventListener("click", function () {
+  document.querySelector(".nav-list").classList.toggle("show");
+  document.querySelector(".overlay").style.display = "block";
+});
+
+document.querySelector(".overlay").addEventListener("click", function () {
+  document.querySelector(".nav-list").classList.remove("show");
+  document.querySelector(".overlay").style.display = "none";
+});
