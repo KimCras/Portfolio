2b90d1262a2cdee8b58ab91f922c3d2986923f17
Kimberly <kira0001@stud.kea.dk>
2024-01-09 00:59:27 +0100

    animation page  done

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ document.querySelector(".overlay").addEventListener("click", function () {
   document.querySelector(".nav-list").classList.remove("show");
   document.querySelector(".overlay").style.display = "none";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".project img");
+  const body = document.body;
+  const main = document.querySelector(".project"); // Select the main element with class "project"
+
+  images.forEach(function (image) {
+    image.addEventListener("click", function () {
+      const isEnlarged = image.classList.toggle("enlarged");
+
+      if (isEnlarged) {
+        body.classList.add("overlay-visible");
+        main.classList.add("overlay-visible"); // Apply the class to main
+      } else {
+        body.classList.remove("overlay-visible");
+        main.classList.remove("overlay-visible"); // Remove the class from main
+      }
+    });
+  });
+});
